6421796192a472001f5a6d7984616b36bc1c1e08
Akhil <dasariakhil2001@gmail.com>
2022-08-05 20:19:12 +0530

    adding bootstrap

--- src/components/ShowOwnBlog.js
+++ src/components/ShowOwnBlog.js
@@ -18,35 +18,42 @@ const ShowOwnBlog = (props) => {
     // }, [])
     // const blogToShow = props.blogs.filter(blog => blog.id == params.id)
     const blog = props.blogs.find((item) => item.id == params.id)
-    
+
     return (
         <>
-        <Header />
+            <Header />
             <div className='blog'>
                 <div>Title: {blog.title}</div>
                 <div>Author: {blog.authorName}</div>
                 <div>Content: {blog.content}</div>
-                {blog.user_id === props.user_id ? 
-                
-                (
-                <>
-                <Link to={`/updateblog/${blog.id}`}>
-                <button>Edit</button>
-                </Link>
-                
-                <Link to={'/getrandomblogs'}>
-                <button onClick={
-                    () => {
-                        console.log('deletig the selected blog')
-                        fetch(props.baseURL + `/blog/${blog.id}`, {method: "DELETE"})
-                    }
-                }>Delete</button></Link>
-                </>) : <div></div>
+                {blog.user_id === props.user_id ?
+                    (
+                        <>
+                            <Link to={`/updateblog/${blog.id}`}>
+                                <button>Edit</button>
+                            </Link>
 
-                
-            }
-                
+                            <Link to={'/getrandomblogs'}>
+                                <button onClick={
+                                    () => {
+                                        console.log('deletig the selected blog')
+                                        fetch(props.baseURL + `/blog/${blog.id}`, { method: "DELETE" })
+                                    }
+                                }>Delete</button></Link>
+                        </>) : <div></div>
+                }
+
             </div>
+
+            {/* <div className="card" style="width: 18rem;">
+                <div className="card-body">
+                    <h5 className="card-title">{blog.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{blog.authorName}</h6>
+                    <p className="card-text">{blog.content}</p>
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
+            </div> */}
         </>
     )
 }
